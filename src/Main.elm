854c17e47fe50d573e05d42125d4c541569474a2
nishiura hiroki <nishiurahiroki@gmail.com>
2020-01-13 05:51:56 -0500

    Change login UI.

--- src/Main.elm
+++ src/Main.elm
@@ -5,23 +5,24 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
-
--- TODO 仮実装
+-- TODO change Browser.application
 
 type alias Model =
   {
-    count : Int
+    loginUserId : String,
+    loginUserPassword : String
   }
 
 type Msg =
-  Increment |
-  Decrement
+  InputLoginUserIdText   String |
+  InputLoginPasswordText String
 
 init : () -> ( Model, Cmd Msg )
 init () =
   (
     {
-      count = 0
+      loginUserId = "",
+      loginUserPassword = ""
     },
     Cmd.none
   )
@@ -30,21 +31,30 @@ init () =
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Increment ->
-      ({ model | count = model.count + 1 }, Cmd.none)
+    InputLoginUserIdText userId ->
+      ({ model | loginUserId = userId}, Cmd.none)
 
-    Decrement ->
-      ({ model | count = model.count - 1 }, Cmd.none)
+    InputLoginPasswordText password ->
+      ({ model | loginUserPassword = password }, Cmd.none)
 
 
 view : Model -> Html Msg
 view model =
   div []
     [
-      button [ onClick Increment ] [ text "+" ],
-      text (String.fromInt model.count),
-      button [ onClick Decrement ] [ text "-" ]
+      div [] [
+        text "ユーザーID : ",
+        input [ type_ "text", value model.loginUserId, onInput InputLoginUserIdText ] []
+      ],
+      div [] [
+        text "パスワード : ",
+        input [ type_ "password", value model.loginUserPassword, onInput InputLoginPasswordText ] []
+      ],
+      div [] [
+        button [] [ text "ログイン" ]
+      ]
     ]
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.none
